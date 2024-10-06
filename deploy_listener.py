c0cb2abbcccc01c31b59ab9489a5519a26d177d7
nekoy <nekoyama3.n@gmail.com>
2024-10-07 01:21:05 +0900

    password check fix

--- deploy_listener.py
+++ deploy_listener.py
@@ -89,4 +89,4 @@ def deploy():
     return jsonify({'status': 'error', 'message': 'Password does not match any machine'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
